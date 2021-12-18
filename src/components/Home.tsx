2d54af45ba7af52b801718a6045b0a1e036425f3
withewolf-dev <itskashyap26@gmail.com>
2021-12-18 13:08:59 +0530

    viewrs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+import Viewrs from "./Viewrs";
 
 interface Props {}
 
 const Home = (props: Props) => {
-  return <div>home</div>;
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewrs />
+    </Container>
+  );
 };
 
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+  background: url("/images/home-background.png") center center / cover no-repeat
+    fixed;
+  /* &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  } */
+`;
 export default Home;
