c22bd11e4d5409fc4037ffbe36e114e482ded15f
withewolf-dev <itskashyap26@gmail.com>
2021-12-20 12:20:34 +0530

    new

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <StyledEngineProvider injectFirst>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
